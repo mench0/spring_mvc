b77555d57b2654dbb81c080eb662c2f8f760b813
mench0 <46263998+mench0@users.noreply.github.com>
2021-01-31 18:34:25 +0300

    2.2.2

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,34 @@
+package web.controller;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.service.CarService;
+
+import java.util.List;
+
+@Controller
+public class CarController {
+
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping(value = "/cars")
+    public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model){
+
+        if (count == null){
+            model.addAttribute("allCars", carService.getCarList());
+        } else {
+            model.addAttribute("allCars", carService.listCar(count));
+        }
+        return "car-page";
+    }
+
+}
---
+++ src/main/java/web/service/CarServiceImp.java
@@ -0,0 +1,29 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CarServiceImp implements CarService{
+
+    @Override
+    public List<Car> getCarList() {
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car("Cadilac", "black", 100));
+        carList.add(new Car("Ford", "Blue", 150));
+        carList.add(new Car("Pantiac", "Red", 200));
+        carList.add(new Car("Chevrolet", "Yellow", 250));
+        carList.add(new Car("Jeep", "Green", 300));
+        return carList;
+    }
+
+    @Override
+    public List<Car> listCar(int num) {
+        return getCarList().stream().limit(num).collect(Collectors.toList());
+    }
+
+}
